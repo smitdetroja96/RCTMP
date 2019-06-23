818496ea50c211b6d99244df38ae2d76c49e30c0
shreyansh08 <shreyansh.s1998@gmail.com>
2019-06-24 00:23:53 +0530

    raxter done

--- app/src/main/java/com/example/rctmp/RaxterSearchActivity.java
+++ app/src/main/java/com/example/rctmp/RaxterSearchActivity.java
@@ -2,6 +2,7 @@ package com.example.rctmp;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.provider.SyncStateContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -63,30 +64,11 @@ public class RaxterSearchActivity extends AppCompatActivity {
         });
 
 
-        webView.setWebViewClient(new WebViewClient(){
-
-            @Override
-            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
-                super.onReceivedError(view, request, error);
-                page_load_error = true;
-            }
-
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                if(!loaded) {
-                    super.onPageFinished(view, url);
-                    if (!page_load_error) {
-                        loaded = true;
-                    }
-                }
-                else {
-                    //Toast.makeText(getApplicationContext(), "Error: Please Check Your Internet Connection!", Toast.LENGTH_SHORT).show();
-                    page_load_error = false;
-                }
-            }
-        });
-
         webSettings = webView.getSettings();
+        if(Build.VERSION.SDK_INT >= 28){
+            webView.setInitialScale(100);
+            webSettings.setBuiltInZoomControls(true);
+        }
         webSettings.setJavaScriptEnabled(true);
         webSettings.setAllowContentAccess(true);
         webSettings.setDomStorageEnabled(true);
