3799b8bb23637eaf53456937b008390008ed2765
gaurang-b <gkb1998@gmail.com>
2019-08-14 00:11:08 +0530

    100% working notification

--- app/src/main/java/com/example/rctmp/ShowNotificationActivity.java
+++ app/src/main/java/com/example/rctmp/ShowNotificationActivity.java
@@ -4,24 +4,60 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class ShowNotificationActivity extends AppCompatActivity {
 
     WebView webView;
+    ProgressBar progressBar;
     WebSettings webSettings;
+    Toolbar web_notif_toolbar;
+    String refresh_url;
+    String downloadURL;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_notification);
+        webView = findViewById(R.id.wv_for_notification);
+
+        web_notif_toolbar = findViewById(R.id.toolbar_web_notif);
+        setSupportActionBar(web_notif_toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Notification");
+
+        progressBar = findViewById(R.id.pb_progressBarNotif);
+        WebSettings ws = webView.getSettings();
+        ws.setJavaScriptEnabled(true);
+
+        webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                progressBar.setProgress(newProgress);
+                if(newProgress==100)
+                    progressBar.setVisibility(View.INVISIBLE);
+                else
+                    progressBar.setVisibility(View.VISIBLE);
+            }
+        });
+
+
 
         java.lang.String def = "https://opac.daiict.ac.in";
         String url = getIntent().getStringExtra("url");
@@ -30,7 +66,67 @@ public class ShowNotificationActivity extends AppCompatActivity {
             url = "http://" + url;
 
         Log.e("thisOpens:",url);
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-        startActivity(browserIntent);
+//        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+//        startActivity(browserIntent);
+        webView.loadUrl(url);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.toolbar_menu,menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int menu_id = item.getItemId();
+        if(menu_id == R.id.toolbar_refresh)
+        {
+            refresh_url = webView.getUrl();
+            webView.loadUrl(refresh_url);
+//            Toast.makeText(this, "Toolbar Menu 1", Toast.LENGTH_SHORT).show();
+        }
+        else if(menu_id == R.id.toolbar_open_browser)
+        {
+            String temp_url = webView.getUrl();
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(temp_url));
+            startActivity(i);
+
+//            Toast.makeText(this, "Toolbar Menu 2", Toast.LENGTH_SHORT).show();
+        }
+        else if(menu_id == R.id.toolbar_share)
+        {
+            try
+            {
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("text/plain");
+                i.putExtra(Intent.EXTRA_SUBJECT, "RC APP");
+
+                String share_string = webView.getUrl();
+
+                String sAux = "RC APP\n";
+                sAux = sAux + share_string;
+
+                i.putExtra(Intent.EXTRA_TEXT, sAux);
+                startActivity(Intent.createChooser(i, "Read..."));
+
+            }
+            catch (Exception e) {}
+//            Toast.makeText(this, "Toolbar Menu 3", Toast.LENGTH_SHORT).show();
+        }
+        else if(menu_id == R.id.home)
+        {
+            Log.e("Hiii","........");
+//            Toast.makeText(this, "Hiii....", Toast.LENGTH_SHORT).show();
+//            this.finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
